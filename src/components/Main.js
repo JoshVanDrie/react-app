06f9528ebcbf587f1a2fc044361bc37bb03210b5
Van Drie Joshua D <VanDrieJoshua@JohnDeere.com>
2022-05-23 10:53:50 -0500

    finished first project

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,18 @@
 import React from "react"
+import ReactLogoLarge from '../images/react-icon-large.png'
 
 export default function Main() {
     return (
         <main>
-            <h1 className="main--title">Fun facts about React</h1>
+            <h1 className="main--title"> Fun facts about react</h1>
             <ul className="main--facts">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100K stars on GitHub</li>
+                <li>Has well over 100K stars on Github</li>
                 <li>Is maintained by Facebook</li>
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
+            {/*<img src={ReactLogoLarge} className="background--icon"/>*/}
         </main>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ import ReactLogoSmall from '../images/react-icon-small.png'
 export default function Navbar() {
     return (
         <nav>
-            <img src= {ReactLogoSmall} className="nav--icon" />
-            <h3 className="nav--logo_text">ReactFacts</h3>
-            <h4 className="nav--title">React Course - Project 1</h4>
+            <img src= {ReactLogoSmall} className="nav--icon" width='50px'/>
+            <h3 className="nav--logo_text"> ReactFacts</h3>
+            <h4 className="nav--title"> React Course - Project 1</h4>
         </nav>
     )
 }
